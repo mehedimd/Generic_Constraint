fe65138e5211d4cb772abcfdf93f5dd54b760d9c
WADA_58 <WADA_58@DESKTOP-5HQIB3T>
2024-01-31 17:32:12 +0600

    Add project files.

---
+++ ConstraintGeneric/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Lifetime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstraintGeneric
+{
+    internal class Student<T> 
+    {
+        public void Display(T msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+    }
+    class Test
+    {
+        public static void Main()
+        {      
+            Student<int> student = new Student<int>();
+            student.Display(50);
+            Student<string> student2 = new Student<string>();
+            student2.Display("Hello World");
+            
+
+            Console.ReadKey();
+        }
+    }
+}
